e86e758823c137d3bbb2b2f5a49ba0f376329d4a
Brandon King <brandonscottking@gmail.com>
2021-07-20 21:45:27 -0400

    added api calls for weather data

--- src/components/search.js
+++ src/components/search.js
@@ -2,47 +2,55 @@ import React, { useState } from 'react';
 import { Button } from 'react-foundation';
 
 const apiKey = '&appid=' + process.env.REACT_APP_API_KEY;
-const apiCity = 'api.openweathermap.org/data/2.5/weather?q=';
-const apiZip = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiCity = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const apiZip = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 const Search = () => {
 
-    const [search, setSearch] = useState('');
+    const [citySearch, setCitySearch] = useState('');
+    const [zipSearch, setZipSearch] = useState('');
 
-    const handleChange = (evt) => {
-        setSearch(evt.target.value)
-        console.log(evt.target.id)
+    const handleCityChange = (evt) => {
+        let value = evt.target.value;
+        let noSpace = value.replace(/\s/g, '')
+        setCitySearch(noSpace.toLowerCase())
+    }
+    const handleZipChange = (evt) => {
+        setZipSearch(evt.target.value)
     }
 
-    const handleSubmit = (evt) => {
-        evt.target.id = 'city-search' ?
-          console.log(apiCity + evt.target.value + apiKey)
-            // .then(res => res.json())
-            // .then(data => console.log(data))
-            // .catch(err => console.log(err))
+    const handleSubmit = () => {
+        if (citySearch && zipSearch) {
+            console.log('ERROR: Choose 1 search method')
+        }
+        citySearch ?
+            fetch(apiCity + citySearch + apiKey)
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log(err))
             :
-          fetch(apiZip + evt.target.value + apiKey)
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
+            fetch(apiZip + zipSearch + apiKey)
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log(err))
     }
 
     return (
         <>
-            <input 
-            id="city-search"
-            className="search-field"
-            type="text"
-            placeholder="Search by City & State" 
-            onChange={handleChange}
+            <input
+                id="city-search"
+                className="search-field"
+                type="text"
+                placeholder="Search by City & State"
+                onChange={handleCityChange}
             />
             <h3>OR</h3>
-            <input 
-            id="zip-search"
-            className="search-field"
-            type="text"
-            placeholder="Search by Zip/Postal Code" 
-            onChange={handleChange}
+            <input
+                id="zip-search"
+                className="search-field"
+                type="text"
+                placeholder="Search by Zip/Postal Code"
+                onChange={handleZipChange}
             />
             <Button isExpanded onClick={handleSubmit}>Search</Button>
         </>
